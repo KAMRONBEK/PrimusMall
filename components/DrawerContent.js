8f685e879673d94f1f3ce1a5ec901bb444080589
KAMURANBEK <kamuranbek1998@gmail.com>
2019-10-28 17:54:33 +0500

    drawer fixed

--- components/DrawerContent.js
+++ components/DrawerContent.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Image} from 'react-native';
 import NavigationServices from '../services/NavigationServices';
 import colors from '../constants/colors';
 import DrawerMenuItem from '../components/DrawerMenuItem';
 import strings from '../localization/strings';
 import Header from '../components/Header';
+import Feather from 'react-native-vector-icons/Feather';
 
 const DrawerContent = ({navigation}) => {
   return (
@@ -15,8 +16,38 @@ const DrawerContent = ({navigation}) => {
           styles.top,
           {
             borderBottomColor: colors.lightGray,
+            backgroundColor: colors.superLightGray,
           },
-        ]}></View>
+        ]}>
+        <View style={styles.nameWrap}>
+          <Text style={styles.name}>АНДРЕЙ</Text>
+          <Text style={styles.name}>КИМ</Text>
+        </View>
+        <View style={styles.imageWithIcon}>
+          <View style={styles.imageWrap}>
+            <Image
+              style={{
+                width: 84,
+                borderRadius: 84,
+                height: 84,
+              }}
+              source={{
+                uri:
+                  'https://www.menshairstylestoday.com/wp-content/uploads/2018/10/Triangle-Face-Shape-Hairstyles-Men.jpg',
+              }}
+            />
+          </View>
+          <View
+            style={[
+              styles.plusIcon,
+              {
+                backgroundColor: colors.superLightGray,
+              },
+            ]}>
+            <Feather name="plus" />
+          </View>
+        </View>
+      </View>
       <View style={styles.bottom}>
         <DrawerMenuItem
           text={strings.shops}
@@ -74,10 +105,39 @@ const DrawerContent = ({navigation}) => {
 
 const styles = StyleSheet.create({
   top: {
+    // width: Dimensions.get("window").width - 60,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     borderBottomWidth: 1,
+    padding: 30,
+  },
+  nameWrap: {
+    marginLeft: 0,
+    flexDirection: 'column',
+  },
+  name: {
+    fontSize: 23,
+    fontWeight: '400',
   },
   bottom: {
     padding: 40,
+  },
+  imageWithIcon: {
+    flexDirection: 'row',
+  },
+  imageWrap: {
+    borderRadius: 80,
+    borderWidth: 5,
+  },
+  plusIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: -70,
+    marginLeft: -26,
+    height: 26,
+    width: 26,
+    borderRadius: 18,
   },
 });
 
--- components/DrawerMenuItem.js
+++ components/DrawerMenuItem.js
@@ -1,36 +1,35 @@
-import React from "react";
-import { Text, View, StyleSheet, TouchableWithoutFeedback } from "react-native";
-import Icon from "../constants/icons";
+import React from 'react';
+import {Text, View, StyleSheet, TouchableWithoutFeedback} from 'react-native';
+import Icon from '../constants/icons';
 
-const DrawerMenuItem = ({ iconName, text, small, onPress }) => {
-	// const { navigate } = navigation;
-	return (
-		<React.Fragment>
-			{/* <TouchableWithoutFeedback onPress={() => navigate(redirectTo, {})}> */}
-			<TouchableWithoutFeedback onPress={onPress}>
-				<View style={styles.container}>
-					<View style={styles.icon}>
-						<Icon name={iconName} size={!small ? 30 : 15} />
-					</View>
-					<Text>{text}</Text>
-				</View>
-			</TouchableWithoutFeedback>
-		</React.Fragment>
-	);
+const DrawerMenuItem = ({iconName, text, small, onPress}) => {
+  // const { navigate } = navigation;
+  return (
+    <React.Fragment>
+      {/* <TouchableWithoutFeedback onPress={() => navigate(redirectTo, {})}> */}
+      <TouchableWithoutFeedback onPress={onPress}>
+        <View style={styles.container}>
+          <View style={styles.icon}>
+            <Icon name={iconName} size={!small ? 30 : 15} />
+          </View>
+          <Text>{text}</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </React.Fragment>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flexDirection: "row",
-		paddingTop: 10,
-		paddingBottom: 10,
-		alignItems: "center",
-		borderWidth: 1
-	},
-	icon: {
-		flexBasis: 50,
-		alignItems: "center"
-	}
+  container: {
+    flexDirection: 'row',
+    paddingTop: 10,
+    paddingBottom: 10,
+    alignItems: 'center',
+  },
+  icon: {
+    flexBasis: 50,
+    alignItems: 'center',
+  },
 });
 
 export default DrawerMenuItem;
